1970adddb031dc89dc860f9a55bf32d2113d0843
lilianabs <lilianabsmath@gmail.com>
2025-11-03 18:12:35 -0600

    Add react graph

--- react_agent.py
+++ react_agent.py
@@ -1,6 +1,86 @@
 import os
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langgraph.graph import MessagesState
+from langgraph.graph import START, StateGraph
+from langgraph.prebuilt import tools_condition, ToolNode
+
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 
 load_dotenv()
 
 API_KEY = os.getenv("OPENAI_API_KEY")
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+
+# Tools to be used by the agent
+def multiply(a: int, b: int) -> int:
+    """Multiply a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+# This will be a tool
+def add(a: int, b: int) -> int:
+    """Adds a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a + b
+
+def divide(a: int, b: int) -> float:
+    """Adds a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a / b
+
+tools = [multiply, add, divide]
+llm_with_tools = llm.bind_tools(tools)
+
+"""
+The state will have 3 types of messages:
+
+- HumanMessage: User input
+- AIMessage: Response from the language model
+- ToolsMessage: Response from the tool
+"""
+
+sys_msg = SystemMessage(content="You are a helpful math assistant performing arithmetic on a set of inputs. Use the tools when needed.")
+
+# Node
+def assistant(state: MessagesState):
+   return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+
+
+# Graph
+builder = StateGraph(MessagesState)
+
+# Define nodes: these do the work
+builder.add_node("assistant", assistant)
+builder.add_node("tools", ToolNode(tools))
+
+# Define edges: these determine the control flow
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges(
+    "assistant",
+    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
+    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
+    tools_condition,
+)
+builder.add_edge("tools", "assistant")
+graph = builder.compile()
+# TODO: Get code to generate image of graph
+
+messages = [HumanMessage(content="Add 3 and 4. Multiply the output by 2. Divide the output by 5")]
+messages = graph.invoke({"messages": messages})
+
+for m in messages['messages']:
+    m.pretty_print()
